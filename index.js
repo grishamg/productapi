8fd6ffbb2a848f6131ab415709ca8ea39123cc55
grishamgarg1234@gmail.com <you@example.com>
2022-04-28 13:38:06 +0530

    remove .env file

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+require( 'dotenv' ).config();
 const express = require( 'express' );
 const mongoose = require( 'mongoose' );
 const app = express();
 const port = process.env.PORT || 5000
 
 // connect to mongodb atlas at 
-mongoose.connect( "mongodb+srv://grishamgarg:<myfirstapi>@cluster0.jfmai.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" );
+mongoose.connect( process.env.MONGODB_URL, { useNewUrlParser: true } ).then( () =>
+{
+    console.log( "Connected in the MongoDB Database" )
+} ).catch( err =>
+{
+    console.log( "Something wrong happened", err );
+} )
 
+// start database
 app.listen( port, () =>
 {
     console.log( `Server started at port ${ port }` )
