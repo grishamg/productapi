14971b7909e02e11825479050a04dc3a34cbe9c7
grishamgarg1234@gmail.com <you@example.com>
2022-04-29 11:35:49 +0530

    Added Validation

--- models/product.js
+++ models/product.js
@@ -1,5 +1,5 @@
 const mongoose = require( 'mongoose' )
-
+const yup = require( 'yup' );
 // create a Schema
 const ProductSchema = new mongoose.Schema( {
     name: {
@@ -8,16 +8,43 @@ const ProductSchema = new mongoose.Schema( {
     },
     description: {
         type: String,
+        maxlength: 20,
+        required: true
     },
     price: {
-        type: Number
+        type: Number,
+        required: true
     },
     image: {
-        type: String
+        type: String,
+        required: true
     },
     rating: {
-        type: Number
+        type: Number,
+        required: true
     }
 } );
 
-module.exports = new mongoose.model( 'Product', ProductSchema );
+const validateProduct = ( product ) =>
+{
+    const schema = yup.object().shape( {
+        name: yup.string().required(),
+        description: yup.string().required().max( 20 ),
+        price: yup.number().required().min( 0 ),
+        rating: yup.number().required().min( 0 ).max( 5 ),
+        image: yup.string().required(),
+    } );
+
+    return schema
+        .validate( product )
+        .then( product => product )
+        .catch( ( err ) =>
+        {
+            return {
+                message: err.message,
+            };
+        } );
+}
+
+exports.Product = new mongoose.model( 'Product', ProductSchema );
+exports.validateProduct = validateProduct;
--- routes/productroute.js
+++ routes/productroute.js
@@ -1,31 +1,32 @@
 const express = require( 'express' );
-const product = require( '../models/product' );
 const router = express.Router();
-const Product = require( '../models/product' )
+const { Product, validateProduct } = require( '../models/product' )
 
 // create a post route for creating a new product
-router.post( '/', ( req, res ) =>
+router.post( '/', async ( req, res ) =>
 {
-    let product = new Product(
+    const error = await validateProduct( req.body );
+    if ( error.message ) res.status( 400 ).send( error.message );
+    product = new Product(
         {
             name: req.body.name,
             description: req.body.description,
             price: req.body.price,
             image: req.body.image,
             rating: req.body.rating
-
         }
     );
 
-    product.save().then( prod =>
-    {
-        res.send( prod );
-    } ).catch( err =>
-    {
-        res.status( 500 ).send( "Product not stored in db" );
-    } )
-
-
+    product
+        .save()
+        .then( ( prod ) =>
+        {
+            res.send( prod );
+        } )
+        .catch( err =>
+        {
+            res.status( 500 ).send( "Product not stored in db" );
+        } )
 
 } );
 
